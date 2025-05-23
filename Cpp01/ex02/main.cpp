0b4ede85d1e095c65fb34f0ea3c35d77e58a7699
cRYP70n-13 <otmane.kimdil@gmail.com>
2025-05-23 23:41:13 +0100

    cpp 01 done

--- Cpp01/ex02/main.cpp
+++ Cpp01/ex02/main.cpp
@@ -8,9 +8,9 @@ int main(void)
 	std::string &str_ref = str;
 
 	std::cout << "The memory address of the string variable: " << &str << std::endl;
-	std::cout << "The memory address held by stringPTR: " << &str_ptr << std::endl;
+	std::cout << "The memory address held by stringPTR: " << str_ptr << std::endl;
 	std::cout << "The memory address held by stringREF: " << &str_ref << std::endl;
 	std::cout << "The value of the string variable: " << str << std::endl;
-	std::cout << "The value pointed to by stringPTR: " << str_ptr << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *str_ptr << std::endl;
 	std::cout << "The value pointed to by stringREF: " << str_ref << std::endl;
 }
