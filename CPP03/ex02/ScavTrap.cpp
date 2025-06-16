da55b503e356532369ee9b0542a0d6508b4a1fe1
Mouad Kimdil <mouad.kimdil7@gmail.com>
2025-06-16 11:04:57 +0100

    finished ex02 from cpp03

---
+++ CPP03/ex02/ScavTrap.cpp
@@ -0,0 +1,45 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap() : ClapTrap() {
+    HitPoint = 100;
+    EnergyPoint = 50;
+    AttackDamage = 20;
+    std::cout << "ScavTrap Default constructor has been called!" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) {
+    HitPoint = 100;
+    EnergyPoint = 50;
+    AttackDamage = 20;
+    std::cout << "ScavTrap Parameterized constructor has been called!" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &other) : ClapTrap(other) {
+    std::cout << "ScavTrap Copy constructor has been called!" << std::endl;
+}
+
+ScavTrap::~ScavTrap() {
+    std::cout << "ScavTrap Destructor has been called!" << std::endl;
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &other) {
+    if (this != &other) {
+        ClapTrap::operator=(other);
+    }
+    return (*this);
+}
+
+void ScavTrap::guardGate() {
+    std::cout << "ScavTrap " << Name << " has entered Gate keeper mode" << std::endl;
+}
+
+void ScavTrap::attack(const std::string &target) {
+    if (EnergyPoint > 0) {
+        EnergyPoint--;
+        std::cout << "ScavTrap " << Name << " attacks " << target 
+            << ", causing " << AttackDamage << " points of damage!" << std::endl;
+    }
+    else {
+        std::cout << "ScavTrap " << Name << " has no energy to attack!" << std::endl;
+    }
+}
---
+++ CPP03/ex02/ScavTrap.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+	public:
+		ScavTrap();
+		ScavTrap( std::string name );
+		ScavTrap( const ScavTrap &other );
+		~ScavTrap();
+		ScavTrap &operator=( const ScavTrap &other );
+
+		void	guardGate();
+		void	attack( const std::string &target );
+};
