7b067f370536e1955412bd7b63226907f3d94181
Mouad Kimdil <mouad.kimdil7@gmail.com>
2025-06-18 22:17:31 +0100

    added the WrongAnimal and WrongCat classes and tests in the main file

--- CPP04/ex00/main.cpp
+++ CPP04/ex00/main.cpp
@@ -1,11 +1,16 @@
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int main() {
     const Animal *meta = new Animal();
     const Animal *j = new Dog();
     const Animal *i = new Cat();
+
+    const WrongAnimal *wrong = new WrongAnimal();
+    const WrongAnimal *x = new WrongCat();
 
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
@@ -13,8 +18,16 @@ int main() {
     i->makeSound(); // will output the dog sound
     meta->makeSound(); // will outout the animal message
 
+
+    std::cout << x->getType() << " " << std::endl;
+    x->makeSound();
+    wrong->makeSound();
+
     delete meta;
     delete i;
     delete j;
+    delete wrong;
+    delete x;
+
     return 0;
 }
